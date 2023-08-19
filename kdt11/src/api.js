0f0ef35cbe4dfa7719429327a8e8cb34ed440a82
dong5397 <dongwok5397@naver.com>
2023-08-20 00:04:53 +0900

    three

---
+++ kdt11/src/api.js
@@ -0,0 +1,12 @@
+const END_POINT = " https://cat-search.edu-api.programmers.co.kr";
+export const request = async (url) => {
+  try {
+    const res = await fetch(`${END_POINT}${url}`);
+    if (!res.ok) {
+      throw new Error("API호출 실패");
+    }
+    return await res.json();
+  } catch (e) {
+    alert(e.message);
+  }
+};
